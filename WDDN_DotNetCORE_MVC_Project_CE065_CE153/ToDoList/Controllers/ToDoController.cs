2e16b35d36409227a456e4b657f25713802e0ed2
jash707 <jash.tamakuwala777@gmail.com>
2021-11-11 15:55:23 +0530

    'Completed'

---
+++ WDDN_DotNetCORE_MVC_Project_CE065_CE153/ToDoList/Controllers/ToDoController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Infrastructure;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ToDoContext context;
+
+        public ToDoController(ToDoContext context)
+        {
+            this.context = context;
+        }
+        //GET /
+        public async Task<ActionResult> Index()
+        {
+            IQueryable<TodoList> items = from i in context.ToDoList orderby i.Id select i;
+            
+            List<TodoList> todoList = await items.ToListAsync();
+
+            return View(todoList);
+        }
+
+        // GET /todo/create
+        public IActionResult Create() => View();
+
+        //POST /todo/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The task has been added!";
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        //GET /todo/edit/id
+        public async Task<ActionResult> Edit(int? id)
+        {
+            TodoList item = await context.ToDoList.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+
+        // POST /todo/edit/id
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Update(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The task has been updated!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        // GET /todo/delete/id
+        public async Task<ActionResult> Delete(int id)
+        {
+            TodoList item = await context.ToDoList.FindAsync(id);
+            if (item == null)
+            {
+                TempData["Error"] = "The task does not exist!";
+            }
+            else
+            {
+                context.ToDoList.Remove(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The task has been deleted!";
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ WDDN_DotNetCORE_MVC_Project_CE065_CE153/ToDoList/Migrations/20211005144350_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoList",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Todo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoList", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoList");
+        }
+    }
+}
